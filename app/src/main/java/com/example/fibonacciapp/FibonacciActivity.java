ba535ad156b5d1f1a521a577ee9311c7a9f8c383
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-09-14 14:36:05 -0500

    Added Fibonacci Activity

--- app/src/main/java/com/example/fibonacciapp/FibonacciActivity.java
+++ app/src/main/java/com/example/fibonacciapp/FibonacciActivity.java
@@ -3,6 +3,9 @@ package com.example.fibonacciapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FibonacciActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class FibonacciActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fibonacci);
+
+        String numFibo = getIntent().getStringExtra("numFibo");
+        Log.i("Numero",numFibo);
+        Integer n = Integer.parseInt(numFibo);
+
+        Fibo(n);
+
     }
+
+    public void Fibo(Integer n)
+    {
+        int t1 = 0, t2 = 1;
+        String num;
+        num = Integer.toString(t1);
+        LinearLayout llFibo = (LinearLayout) findViewById(R.id.llFibo);
+        TextView textViewNum = new TextView(this);
+        textViewNum.setText(num);
+        textViewNum.setTextSize(30);
+        llFibo.addView(textViewNum);
+        for (int i = 1; i <= n; ++i)
+        {
+            num = Integer.toString(t1);
+            TextView textViewNum2 = new TextView(this);
+            textViewNum2 .setText(num);
+            textViewNum2.setTextSize(30);
+            llFibo.addView(textViewNum2);
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/fibonacciapp/MainActivity.java
+++ app/src/main/java/com/example/fibonacciapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         btnFact = findViewById(R.id.btbFact);
         btnFibo = findViewById(R.id.btnFibo);
         btnPaises = findViewById(R.id.btnPaises);
+        edtFiboNumber = findViewById(R.id.edtFiboNumber);
+
+
+        btnFibo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(),FibonacciActivity.class);
+                String num = edtFiboNumber.getText().toString();
+                intent.putExtra("numFibo", num);
+                startActivity(intent);
+            }
+        });
+
 
         btnFact.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +43,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
     }
 }
