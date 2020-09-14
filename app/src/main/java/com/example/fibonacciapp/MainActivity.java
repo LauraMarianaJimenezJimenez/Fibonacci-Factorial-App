7f32efcb0443572dffd5afac1570a374a26c629f
M <monik9912@hotmail.com>
2020-09-14 11:35:39 -0500

    Fixed Main Activity
    change textView to a EditText, inflate elements

--- app/src/main/java/com/example/fibonacciapp/MainActivity.java
+++ app/src/main/java/com/example/fibonacciapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.fibonacciapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnFibo, btnFact, btnPaises;
+    Spinner spnNumeros;
+    EditText edtFiboNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFact = findViewById(R.id.btbFact);
+        btnFibo = findViewById(R.id.btnFibo);
+        btnPaises = findViewById(R.id.btnPaises);
     }
 }
