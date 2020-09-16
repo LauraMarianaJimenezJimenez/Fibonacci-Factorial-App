b4c9ec5458fe712d4e3d5355fde00512deeab9e4
M <monik9912@hotmail.com>
2020-09-15 20:23:47 -0500

    Added toast in main activity

--- app/src/main/java/com/example/fibonacciapp/MainActivity.java
+++ app/src/main/java/com/example/fibonacciapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -43,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
         btnFibo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                
+                String num = edtFiboNumber.getText().toString();
+                if(num.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Ingrese número Fibonacci", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Intent intent = new Intent(view.getContext(),FibonacciActivity.class);
+                    intent.putExtra("numFibo", num);
+                    startActivity(intent);
+                }
 
 
                 String num = edtFiboNumber.getText().toString();
