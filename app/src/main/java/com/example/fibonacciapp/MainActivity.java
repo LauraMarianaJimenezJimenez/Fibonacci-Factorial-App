b656dc82884a0b6dd884d57aad055448e2f028d8
M <monik9912@hotmail.com>
2020-09-15 20:16:59 -0500

    Added counts on Main Activity

--- app/src/main/java/com/example/fibonacciapp/MainActivity.java
+++ app/src/main/java/com/example/fibonacciapp/MainActivity.java
@@ -9,11 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     Button btnFibo, btnFact, btnPaises;
     Spinner spnNumeros;
     EditText edtFiboNumber;
+    TextView txtCountFacto, txtCountFibo, txtCountPaises, txtFechaFibo, txtFechaFacto, txtFechaPaises;
+    boolean rFibonacci = false, rFactorial = false, rPaises = false;
+    int countFibo = 0, countFacto = 0, countPaises = 0;
+    String dtFibo, dtFacto, dtPaises;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +33,12 @@ public class MainActivity extends AppCompatActivity {
         edtFiboNumber = findViewById(R.id.edtFiboNumber);
         edtFiboNumber = findViewById(R.id.edtFiboNumber);
         spnNumeros = findViewById(R.id.spnNumeros);
-
+        txtCountFacto = findViewById(R.id.txtCountFacto);
+        txtCountFibo = findViewById(R.id.txtCountFibo);
+        txtCountPaises = findViewById(R.id.txtCountPaises);
+        txtFechaFibo = findViewById(R.id.txtFechaFibo);
+        txtFechaFacto = findViewById( R.id.txtFechaFacto);
+        txtFechaPaises = findViewById(R.id.txtFechaPaises);
 
         btnFibo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(view.getContext(),FibonacciActivity.class);
                 String num = edtFiboNumber.getText().toString();
                 intent.putExtra("numFibo", num);
+                rFibonacci = true;
                 startActivity(intent);
             }
         });
@@ -44,9 +59,41 @@ public class MainActivity extends AppCompatActivity {
                 String factorialString = spnNumeros.getSelectedItem().toString();
                 int factorial = Integer.parseInt(factorialString);
                 i.putExtra("factorial", factorial);
+                rFactorial = true;
                 startActivity(i);
             }
         });
 
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(rFibonacci == true){
+            countFibo ++;
+            Date cal = (Date) Calendar.getInstance().getTime();
+            dtFibo = cal.toLocaleString();
+            txtCountFibo.setText("# Fibonacci: " + countFibo);
+            txtFechaFibo.setText("Última vez: " + dtFibo.toString());
+            rFibonacci = false;
+        }
+
+        if(rFactorial == true){
+            countFacto ++;
+            Date cal = (Date) Calendar.getInstance().getTime();
+            dtFacto = cal.toLocaleString();
+            txtCountFacto.setText("# Factorial: " + countFacto);
+            txtFechaFacto.setText("Última vez: " + dtFacto.toString());
+            rFactorial = false;
+        }
+
+        if(rFibonacci == true){
+            countPaises ++;
+            Date cal = (Date) Calendar.getInstance().getTime();
+            dtPaises = cal.toLocaleString();
+            txtCountFibo.setText("# Países: " + countPaises);
+            txtFechaPaises.setText("Última vez: "+ dtPaises);
+            rPaises = false;
+        }
     }
 }
